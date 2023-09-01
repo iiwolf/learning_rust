7654e1747b4c798b1158a0f0617c7f27cc72efe3
Isaac Wolf <ijwolf8@gmail.com>
2023-09-01 07:11:58 -0700

    feat: into_iter

--- src/second.rs
+++ src/second.rs
@@ -9,6 +9,8 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -30,6 +32,30 @@ impl<T> List<T> {
             node.elem
         })
 
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -72,5 +98,49 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        let peek_val = list.peek_mut();
+        *peek_val.unwrap() = 99;
+        assert_eq!(list.peek(), Some(&99));
+
+        *list.peek_mut().unwrap() = 98;
+        assert_eq!(list.peek(), Some(&98));
+
+        list.peek_mut().map(|value| {
+            *value = 97
+        });
+        assert_eq!(list.peek(), Some(&97));
+
+        // Peek when it's None to get an idea of how map works
+        let mut list = List::new();
+        list.peek_mut().map(|value| {
+            *value = 97
+        });
+        list.peek_mut().map(|value| {
+            *value = 96
+        });
+        assert_eq!(list.peek(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);    
     }
 }
