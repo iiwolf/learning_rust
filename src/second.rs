1f6543427fcfde4ae8a5a3f7f713b136241f438c
Isaac Wolf <isaac@Isaacs-MacBook-Pro.local>
2023-09-02 10:16:11 -0700

    feat: iter

--- src/second.rs
+++ src/second.rs
@@ -11,6 +11,10 @@ struct Node<T> {
 
 pub struct IntoIter<T>(List<T>);
 
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -50,12 +54,28 @@ impl<T> List<T> {
         IntoIter(self)
     }
 
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_deref()
+        }
+    }
+
 }
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
     }
 }
 
@@ -143,4 +163,16 @@ mod test {
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);    
     }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
 }
