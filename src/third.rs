fea55b844dbf80a3f0ad592c99f9b154d80ed116
Isaac Wolf <ijwolf8@gmail.com>
2023-10-15 17:50:18 -0700

    feat: third.rs

--- src/third.rs
+++ src/third.rs
@@ -1,33 +1,61 @@
 use std::rc::Rc;
 
 pub struct List<T> {
-    head: Link<T>
+    head: Link<T>,
 }
 
 type Link<T> = Option<Rc<Node<T>>>;
 
-struct Node<T> {
-    next: Link<T>,
-    elem: T
+pub struct Node<T> {
+    elem: T,
+    next: Link<T>
 }
 
 impl<T> List<T> {
-
     pub fn new() -> Self {
-        List { head: None}
+        List{ head: None }
     }
 
     pub fn prepend(&self, elem: T) -> List<T> {
-        List {head: Some(
-            Rc::new(
-                Node {next: self.head.clone(),
-                elem: elem}
-            ))
+        List{ 
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone()
+            }))
         }
     }
 
     pub fn tail(&self) -> List<T> {
-        List{head: self.head.as_ref().and_then(|node| node.next.clone())}
+        List {
+            head: self.head.clone()
+        }
     }
-    
+
+}
+
+mod test {
+    use super::{List, Node};
+    use std::rc::Rc;
+
+    #[test]
+    fn test_prepend() {
+        let list: List<i32> = List{ head: Some(Rc::new(Node {elem: 5, next: None}))};
+        let list2 = list.prepend(6);
+        list.prepend(7);
+
+        assert_eq!(list.head.as_ref().unwrap().elem, 5);
+        assert_eq!(list.head.as_ref().unwrap().elem, 5);
+        // assert_eq!(list.head.as_ref().unwrap().next, );
+
+        // println!("Elem: {}", list.head.as_ref().unwrap().elem);
+        // println!("Elem: {}", list.head.as_ref().unwrap().next.as_ref().unwrap().elem);
+
+
+        // println!("Strong count: {}", Rc::strong_count(&list.head.unwrap()));
+                // let list2 = list.prepend(6);
+        // let list3 = list.prepend(7);        // assert_eq!(list.head.clone().unwrap().elem, 5);
+        // assert_eq!(list.head.clone().unwrap().elem, 5);
+        // assert_eq!(list.head.unwrap().elem, 5);
+
+    }
 }
