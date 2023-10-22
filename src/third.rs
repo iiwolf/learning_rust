53e4ba0398eebf7e8f0eccf78c1bc933533aa663
Isaac Wolf <ijwolf8@gmail.com>
2023-10-21 20:56:59 -0700

    feat: tail! ...again... maybe I understand it this time

--- src/third.rs
+++ src/third.rs
@@ -27,7 +27,7 @@ impl<T> List<T> {
 
     pub fn tail(&self) -> List<T> {
         List {
-            head: self.head.clone()
+            head: self.head.as_ref().and_then(|node| node.next.clone())
         }
     }
 
@@ -39,12 +39,18 @@ mod test {
 
     #[test]
     fn test_prepend() {
-        let list: List<i32> = List{ head: Some(Rc::new(Node {elem: 5, next: None}))};
-        let list2 = list.prepend(6);
-        list.prepend(7);
+        let mut list: List<i32> = List{ head: Some(Rc::new(Node {elem: 5, next: None}))};
+        assert_eq!(list.head.as_ref().unwrap().elem, 5);
 
-        assert_eq!(list.head.as_ref().unwrap().elem, 5);
-        assert_eq!(list.head.as_ref().unwrap().elem, 5);
+        list = list.prepend(6);
+        assert_eq!(list.head.as_ref().unwrap().elem, 6);
+
+        list = list.prepend(7);
+        assert_eq!(list.head.as_ref().unwrap().elem, 7);
+
+        
+        // assert_eq!(list.head.as_ref().unwrap().elem, 5);
+        // assert_eq!(list.head.as_ref().unwrap().elem, 5);
         // assert_eq!(list.head.as_ref().unwrap().next, );
 
         // println!("Elem: {}", list.head.as_ref().unwrap().elem);
@@ -56,6 +62,32 @@ mod test {
         // let list3 = list.prepend(7);        // assert_eq!(list.head.clone().unwrap().elem, 5);
         // assert_eq!(list.head.clone().unwrap().elem, 5);
         // assert_eq!(list.head.unwrap().elem, 5);
+
+    }
+
+    #[test]
+    fn test_tail() {
+        let mut list: List<i32> = List { head: Some(Rc::new(Node {elem: 5, next: None}))};
+        list = list.prepend(4);
+        list = list.prepend(3);
+        list = list.prepend(2);
+        let new_list = list.tail();
+        assert_eq!(new_list.head.as_ref().unwrap().elem, 3);
+        let new_list = list.tail().tail();
+        assert_eq!(new_list.head.as_ref().unwrap().elem, 4);
+        
+        let new_list = list.tail().tail().tail();
+        assert_eq!(new_list.head.as_ref().unwrap().elem, 5);
+        
+        // Will break unless you use map instead of unwrap!
+        let new_list = list.tail().tail().tail().tail().tail();
+        assert!(new_list.head.is_none());
+        // new_list.head.as_ref().unwrap().elem;
+        // for i in 1..4 { 
+        //     println!("Elem {}", trunc_list)
+        // }
+        // assert_eq!(list.tail().tail().head.unwrap().elem, 4);
+
 
     }
 }
